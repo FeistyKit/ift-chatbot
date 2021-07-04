a577f039264952b669742b292cb7b5ff35a1d59c
FeistyKit <eeveebeevee33@gmail.com>
2021-07-03 20:16:03 -0400

    got basic connecting working

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,9 @@ use std::{
     },
 };
 
+use tokio::sync::mpsc::UnboundedReceiver;
+use twitch_irc::{ClientConfig, SecureTCPTransport, TwitchIRCClient, login::StaticLoginCredentials, message::ServerMessage};
+
 use crate::input::input_thread;
 type ThreadedMap = Arc<Mutex<HashMap<String, BetDetails>>>;
 
@@ -21,12 +24,21 @@ struct BetDetails {
     times_right: u8,
 }
 
-fn main() {
-    let (handle, rx) = input_thread();
-    while let Ok(event) = rx.recv() {
-        println!("Event received: {:?}", event);
+#[tokio::main]
+async fn main() {
+    if !cfg!(debug_assertions) {
+        panic!("Don't forget to change to keshy's acct!");
     }
-    handle.join().unwrap();
+    let (input_handle, _rx) = input_thread();
+    let (mut incoming_messages, client) = prepare_client();
+    let message_handle = tokio::spawn(async move {
+        while let Some(message) = incoming_messages.recv().await {
+            println!("{:?}", message);
+        }
+    });
+    client.join("feistyshade".to_string());
+    input_handle.join().unwrap();
+    message_handle.await.unwrap();
 }
 
 fn get_from_file() -> (String, String) {
@@ -37,4 +49,12 @@ fn get_from_file() -> (String, String) {
         "First part of lines is not proper oauth identification!"
     );
     (lines[0].replace("oauth:", ""), lines[1].to_string())
+}
+
+
+fn prepare_client() -> (UnboundedReceiver<ServerMessage>, TwitchIRCClient<SecureTCPTransport, StaticLoginCredentials>) {
+    let raw_credentials = get_from_file();
+    let creds = StaticLoginCredentials::new("iftBot".to_string(), Some(raw_credentials.0));
+    let config = ClientConfig::new_simple(creds);
+    TwitchIRCClient::new(config)
 }
